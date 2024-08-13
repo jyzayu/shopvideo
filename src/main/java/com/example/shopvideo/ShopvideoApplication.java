df6bc85f67e908802da990aae279ed7acc82772a
jyzayu <moosou30@gmail.com>
2024-08-13 11:06:06 +0900

    first commit

---
+++ src/main/java/com/example/shopvideo/ShopvideoApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopvideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopvideoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shopvideo/ShopvideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopvideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
